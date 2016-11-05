65d51fd86607593f392f21b33e361e2c5c841ee7
Michal B <michal2229@gmail.com>
2016-11-05 15:46:25 +0100

    opengl cumpute shaders with glutin

---
+++ rust_glium_glutin_compute/README.md
@@ -0,0 +1,5 @@
+## Compute Shader test
+
+Initial code based on [glium example](https://github.com/tomaka/glium/blob/master/examples/gpgpu.rs)
+
+There is an error related to [issue 1310](https://github.com/tomaka/glium/issues/1310) because of linux graphics stack with Nvidia binary driver (and probably also optimus support).
---
+++ rust_glium_glutin_compute/src/main.rs
@@ -0,0 +1,58 @@
+#[macro_use]
+extern crate glium;
+extern crate rand;
+use glium::glutin;
+
+fn main() {
+    use glium::DisplayBuild;
+
+    let display = glutin::HeadlessRendererBuilder::new(1024, 1024)
+        .build_glium()
+        .unwrap();
+
+    let program = glium::program::ComputeShader::from_source(&display, r#"\
+            #version 430
+            layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+            layout(std140) buffer MyBlock {
+                float power;
+                float values[256];
+            };
+            void main() {
+                float val = values[gl_GlobalInvocationID.x];
+                values[gl_GlobalInvocationID.x] = pow(val, power);
+            }
+        "#).unwrap();
+
+    struct Data {
+        power: f32,
+        _padding: [f32; 3],
+        values: [f32],
+    }
+
+    implement_buffer_content!(Data);
+    implement_uniform_block!(Data, power, values);
+
+    const NUM_VALUES: usize = 4096;
+
+    let mut buffer: glium::uniforms::UniformBuffer<Data> =
+              glium::uniforms::UniformBuffer::empty_unsized(&display, 4 + 4 * NUM_VALUES).unwrap();
+
+    {
+        let mut mapping = buffer.map();
+        mapping.power = rand::random();
+        for val in mapping.values.iter_mut() {
+            *val = rand::random();
+        }
+    }
+
+    program.execute(uniform! { MyBlock: &*buffer }, 4096, 1, 1);
+
+    {
+        let mapping = buffer.map();
+        println!("Power is: {:?}", mapping.power);
+        for val in mapping.values.iter().take(10) {
+            println!("{:?}", *val);
+        }
+        println!("...");
+    }
+}
