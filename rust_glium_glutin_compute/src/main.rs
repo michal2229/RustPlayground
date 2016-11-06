5bb00c252953be8c6aec51999f14b5031526c212
Michal B <michal2229@gmail.com>
2016-11-06 12:43:12 +0100

    c.shader: two more buffers for testing

--- rust_glium_glutin_compute/src/main.rs
+++ rust_glium_glutin_compute/src/main.rs
@@ -2,6 +2,7 @@
 extern crate glium;
 extern crate rand;
 use glium::glutin;
+//use std::iter::Zip;
 
 fn main() {
     use glium::DisplayBuild;
@@ -26,46 +27,64 @@ fn main() {
             layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
             layout(std430) buffer MyBlock {
                 float power;
-                float values[];
+                float values_in[32];
+                uint values_mid[32]; //just for test
+                float values_out[32];
             };
             void main() {
-                float val = values[gl_GlobalInvocationID.x];
-                values[gl_GlobalInvocationID.x] = pow(val, power);
+                uint i = gl_GlobalInvocationID.x;
+                
+                float val = values_in[i];
+                values_out[i] = pow(val, power);
+                
+                values_mid[i] = i*10; //just for test
             }
         "#).unwrap();
 
     struct Data {
         power: f32,
         //_padding: [f32; 3] // not needed anymore, since std430 layout is used instead of std140
-        values: [f32],
+        values_in: [f32;32],
+        values_mid: [u32;32], //just for test
+        values_out: [f32],
     }
 
     implement_buffer_content!(Data);
-    implement_uniform_block!(Data, power, values);
+    implement_uniform_block!(Data, power, values_in, values_mid, values_out);
 
     const NUM_VALUES: usize = 32;
-
+    
+    let bytes_to_allocate: usize = 4 + 4 * NUM_VALUES * 3;
     let mut buffer: glium::uniforms::UniformBuffer<Data> =
-              glium::uniforms::UniformBuffer::empty_unsized(&display, 4 + 4 * NUM_VALUES).unwrap();
+              glium::uniforms::UniformBuffer::empty_unsized(&display, bytes_to_allocate).unwrap();
 
     {
         let mut mapping = buffer.map();
         mapping.power = 1.01;
-        for (i, val) in mapping.values.iter_mut().enumerate() {
+        for (i, val) in mapping.values_in.iter_mut().enumerate() {
             //*val = rand::random();
             *val = i as f32;
         }
+//        for (i, val) in mapping.values_out.iter_mut().enumerate() {
+//            *val = 0.0;
+//        }
     }
 
     program.execute(uniform! { MyBlock: &*buffer }, NUM_VALUES as u32, 1, 1); // Nx1x1 space
 
     {
         let mapping = buffer.map();
-        println!("Power is: {:?}", mapping.power);
         
+        let p = mapping.power;
+        println!("Power is: {:?}\n", p);
         //for val in mapping.values.iter().take(25) { // I want to see them all!
-        for val in mapping.values.iter() {
-            println!("{:?}", *val);
+        for (vi, vo) in mapping.values_in.iter().zip(mapping.values_out.iter()) {
+            println!("{:?}^{:?} = {:?}", *vi, p, *vo);
+        }
+        
+        println!("\nmid buffer");
+        for vm in mapping.values_mid.iter() {
+            println!("{:?}", *vm);
         }
     }
 }
