94f8c7550cdcbbade16bab225d1e6b613d152c1d
Michal B <michal2229@gmail.com>
2016-11-06 03:17:50 +0100

    comp. shader: change of memory layout, some changes (elements vaue, el. number)

--- rust_glium_glutin_compute/README.md
+++ rust_glium_glutin_compute/README.md
@@ -2,6 +2,8 @@
 
 Initial code is based on [glium example](https://github.com/tomaka/glium/blob/master/examples/gpgpu.rs).
 
+### getting rid of BackendCreationError(NotSupported)
+
 There is an [error](https://raw.githubusercontent.com/michal2229/Rust-playground/master/rust_glium_glutin_compute/results/runlogerror.log) related to [issue 1310](https://github.com/tomaka/glium/issues/1310) because of linux graphics stack with Nvidia binary driver (and probably also optimus support on my laptop) - basically it can not be ran in headless mode. 
 
 I had to build a window in order to make it run properly on Nvidia GPU. Intel GPU's drivers are on OpenGL 3.3 only (there are plans to support 4.3 in the future), so no compute shaders support on this GPU. 
@@ -9,3 +11,16 @@ I had to build a window in order to make it run properly on Nvidia GPU. Intel GP
 With a window build fix it seems to run properly on Nvidia binary driver (with optirun). 
 
 ![screen0](https://raw.githubusercontent.com/michal2229/Rust-playground/master/rust_glium_glutin_compute/results/screen0.png)
+
+### fixing memory layout
+
+In the example one could notice, that not all numbers were calculated, but every fourh. 
+It was because of used memory layout: *std140*, which [rounds up size of every element to the size of a vec4](https://www.opengl.org/wiki/Interface_Block_(GLSL))​ (16B).
+
+![screen1](https://raw.githubusercontent.com/michal2229/Rust-playground/master/rust_glium_glutin_compute/results/screen1.png)
+
+After changing layout from *std140* to *std430* things behave a lot better.
+
+![screen2](https://raw.githubusercontent.com/michal2229/Rust-playground/master/rust_glium_glutin_compute/results/screen2.png)
+
+
--- rust_glium_glutin_compute/src/main.rs
+++ rust_glium_glutin_compute/src/main.rs
@@ -6,6 +6,7 @@ use glium::glutin;
 fn main() {
     use glium::DisplayBuild;
 
+    // building new display
     let display = glutin::WindowBuilder::new()
         .with_depth_buffer(24)
         .with_gl_profile(glutin::GlProfile::Core)
@@ -13,12 +14,19 @@ fn main() {
         .build_glium()
         .unwrap();
 
+    // NOTE from https://www.opengl.org/wiki/Interface_Block_(GLSL) about std140 layout: 
+    // "The array stride (the bytes between array elements) is 
+    // always rounded up to the size of a vec4​ (ie: 16-bytes). 
+    // So arrays will only match their C/C++ definitions if the type 
+    // is a multiple of 16 bytes" 
+    // std140 layout was used in this example and memory usage was 4x higher than needed, 
+    // I changed it to std430 which do not waste memory (rounding up to 4 bytes = float is 4 bytes)
     let program = glium::program::ComputeShader::from_source(&display, r#"\
             #version 430
             layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
-            layout(std140) buffer MyBlock {
+            layout(std430) buffer MyBlock {
                 float power;
-                float values[256];
+                float values[24];
             };
             void main() {
                 float val = values[gl_GlobalInvocationID.x];
@@ -28,34 +36,36 @@ fn main() {
 
     struct Data {
         power: f32,
-        _padding: [f32; 3],
+        //_padding: [f32; 3] // not needed anymore, since std430 layout is used instead of std140
         values: [f32],
     }
 
     implement_buffer_content!(Data);
     implement_uniform_block!(Data, power, values);
 
-    const NUM_VALUES: usize = 4096;
+    const NUM_VALUES: usize = 32;
 
     let mut buffer: glium::uniforms::UniformBuffer<Data> =
               glium::uniforms::UniformBuffer::empty_unsized(&display, 4 + 4 * NUM_VALUES).unwrap();
 
     {
         let mut mapping = buffer.map();
-        mapping.power = rand::random();
-        for val in mapping.values.iter_mut() {
-            *val = rand::random();
+        mapping.power = 1.01;
+        for (i, val) in mapping.values.iter_mut().enumerate() {
+            //*val = rand::random();
+            *val = i as f32;
         }
     }
 
-    program.execute(uniform! { MyBlock: &*buffer }, 4096, 1, 1);
+    program.execute(uniform! { MyBlock: &*buffer }, NUM_VALUES as u32, 1, 1); // Nx1x1 space
 
     {
         let mapping = buffer.map();
         println!("Power is: {:?}", mapping.power);
-        for val in mapping.values.iter().take(10) {
+        
+        //for val in mapping.values.iter().take(25) { // I want to see them all!
+        for val in mapping.values.iter() {
             println!("{:?}", *val);
         }
-        println!("...");
     }
 }
