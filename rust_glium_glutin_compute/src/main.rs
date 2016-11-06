24bd04c66410b86543bb62ac34add85b109d61e6
Michal B <michal2229@gmail.com>
2016-11-06 12:16:02 +0100

    i think it is not needed anymore for this type of buffer

--- rust_glium_glutin_compute/src/main.rs
+++ rust_glium_glutin_compute/src/main.rs
@@ -26,7 +26,7 @@ fn main() {
             layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
             layout(std430) buffer MyBlock {
                 float power;
-                float values[24];
+                float values[];
             };
             void main() {
                 float val = values[gl_GlobalInvocationID.x];
